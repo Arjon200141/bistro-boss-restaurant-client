eb0e725d5492ceb450b0de0e0f4112483d9ca9c0
Arjon200141 <aorjon123@gmail.com>
2024-05-22 23:34:54 +0600

    Restaurant service details added

---
+++ src/pages/Home/DetailsREstaurant.jsx/DetailsRestaurant.jsx
@@ -0,0 +1,18 @@
+
+const DetailsRestaurant = () => {
+    return (
+        <div className="my-24 mx-[100px] cinzel-font">
+            <div className="hero h-[450px]" style={{ backgroundImage: 'url(https://i.ibb.co/hLWm8Hh/chef-service.jpg)' }}>
+                <div className="hero-overlay bg-opacity-20"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="bg-white/65 p-8 rounded-lg shadow-lg mx-20">
+                        <h2 className="text-[#151515] font-medium text-4xl mb-4">Bistro Boss</h2>
+                        <p className="text-[#151515] font-medium text-lg ">The BISTRO BOSS Restaurant website offers online reservations for seamless dining planning, a detailed menu showcasing their culinary offerings. Additionally, it provides an online ordering system for takeout and delivery, ensuring customers can enjoy their meals at home.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DetailsRestaurant;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import DetailsRestaurant from "../DetailsREstaurant.jsx/DetailsRestaurant";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <DetailsRestaurant></DetailsRestaurant>
         </div>
     );
 };
